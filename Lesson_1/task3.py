37964f82a5e09f39f3aa60751ce36529ed8ebf23
Your Name <you@example.com>
2022-10-22 22:25:16 +0300

    initial commit

---
+++ Lesson_1/task3.py
@@ -0,0 +1,10 @@
+Seconds = int (input ("Enter number of seconds: ") )
+Seconds = Seconds % (24 * 3600)
+Hour = Seconds // 3600
+Seconds %= 3600
+Minutes = Seconds // 60
+Seconds %= 60
+Days = Seconds % 86400
+print ("Days:",Days,"Hours:",Hour,"Minutes:",Minutes,"Seconds:",Seconds);
+"Days:",Days
+"Minutes:",Minutes
